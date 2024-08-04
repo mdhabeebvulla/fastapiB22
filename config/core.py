b72299a91faeced55a9381317414c92cc6ec236f
mdhabeebvulla <55741151+mdhabeebvulla@users.noreply.github.com>
2024-08-04 11:51:53 +0530

    Add files via upload

---
+++ config/core.py
@@ -0,0 +1,12 @@
+#config/core.py
+import yaml
+
+class Config:
+    def __init__(self,config_file='config.yml'):
+        with open(config_file,'r') as file:
+            self.config = yaml.safe_load(file)
+    def get(self,key,default=None):
+        return self.config.get(key,default)
+        
+
+config = Config()
